298c69733bcb8fb7f24f7c629c2e99f9a9d6f498
Joseph Palermo <josephpalermo@Josephs-MacBook-Air.local>
2017-03-05 11:05:38 -0500

    add an AI that plays optimally using the minimax algorithm + some cleanup

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,5 @@
 from tic_tac_toe_utilities import *
+from GameNode import *
 
 # play tic tac toe
 
@@ -11,13 +12,25 @@ def play():
     turn_of = 'player_1'
     game_over = False
 
+    # build the game tree
+    root_node = GameNode(init_state(), 'player_1', None)
+    root_node.generate_game_tree()
+    game_node = root_node
+
     # game loop --------------------------------------------------------------------
     while not game_over:
-        (x,y) = get_move(turn_of, player_type, state) # loop until a valid move is selected
+        # get a move from the current player
+        move = get_move(turn_of, player_type, state, game_node)
+        (x,y) = move
+        # update state with the move
         state[x][y] = player_symbols[turn_of]
+        # update the current node of the game tree
+        game_node = game_node.get_successor_node(move)
+        # switch player
         turn_of = switch_player(turn_of)
         print_state(state)
-        game_over = terminal_test(state)
+        # test if a terminal state has been reached
+        game_over = terminal_test(state, printResult=True)
 
 
 play()
