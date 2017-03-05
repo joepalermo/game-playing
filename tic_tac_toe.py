871e150baa5d58e008f79b9f296be9f3aa0c1364
Joseph Palermo <josephpalermo@Josephs-MacBook-Air.local>
2017-03-05 09:28:52 -0500

    refactor - remove global state by wrapping initializations in a function, and make corresponding necessary changes + some cleanup

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,11 @@
+# play tic tac toe
+
 from random import randint
+from operator import attrgetter
 
 # simple utility functions ------------------------------------------------------------
 
-# return an empty 3x3 state, represented by a list of lists with only None values
+# return an empty 3x3 grid, represented by a list of lists with only None values
 def init_state():
     state = []
     for _ in range(3):
@@ -24,37 +27,25 @@ def switch_player(turn_of):
     else:
         return 'player_1'
 
-# get the player who uses a particular symbol
-def from_symbol_to_player(symbol):
-    for player in player_symbols.keys():
-        if player_symbols[player] == symbol:
-            return player
-    # this line should never be reached, if it is, then symbol doesn't match
-    # any player in player_symbols
-    raise Exception("symbol " + symbol + ", doens't correspond to a player")
-
 # utility functions to get player moves ----------------------------------------
 
 # get a player's next move as a tuple of integer values
-def get_move(turn_of, state):
+def get_move(turn_of, player_type, state):
     move = None
     # loop until a valid move is selected (i.e. until move is not None)
     while not move:
         if player_type[turn_of] == 'human':
             raw_move_input = raw_input('\n' + turn_of + ', enter your move:')
-            move = valid_raw_move_input(raw_move_input, state)
+            move = parse_raw_move_input(raw_move_input)
             if move and not valid_move(move, state):
                 move = None
         else:
-            #todo
-            #move = get_optimal_move(turn_of, state)
             move = get_random_move(turn_of, state)
     return move
 
-
 # if raw_move_input represents a valid move, then return a parsed representation
 # of it as a tuple of integer values; otherwise, return None
-def valid_raw_move_input(raw_move_input, state):
+def parse_raw_move_input(raw_move_input):
     move = None
     # check that a comma was supplied to separate values
     if ',' in raw_move_input:
@@ -77,7 +68,7 @@ def valid_raw_move_input(raw_move_input, state):
         print '\n' + 'Invalid input. Remember to separate values by a comma'
     return move
 
-# validate a move
+# validate a move against the current state
 def valid_move(move, state):
     (x, y) = move
     is_valid_move = not state[x][y]
@@ -87,18 +78,17 @@ def valid_move(move, state):
 
 # get a list of valid moves
 def get_valid_moves(state):
-    valid_moves = []
-    for i in range(0,3):
-        for j in range(0,3):
-            if not state[i][j]:
-                valid_moves.append((i,j))
-    return valid_moves
+    all_positions = [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+    return [(x,y) for (x,y) in all_positions if not state[x][y]]
 
 # get a random valid move
 def get_random_move(turn_of, state):
     valid_moves = get_valid_moves(state)
     move_index = randint(0,len(valid_moves)-1)
     return valid_moves[move_index]
+
+# todo
+#def get_optimal_move(turn_of, state)
 
 # utility functions to check end-game conditions -------------------------------
 
@@ -107,7 +97,7 @@ def get_random_move(turn_of, state):
 #   - there is a row, column, or diagonal containing 3 of only 1 kind of symbol
 #    (i.e. a player has won)
 #   - or, symbols have been placed in all 9 positions (i.e. tie game)
-def check_if_done(state):
+def terminal_test(state):
     done = False
     winner = get_winner(state)
     # if winner is not None, then announce them as a winner and return True
@@ -126,32 +116,36 @@ def check_if_done(state):
 # if player_2 has won the game, return player_2
 # else return None
 def get_winner(state):
+    winners_symbol = None
     # check the rows
     for i in range(0, len(state)):
         if check_squares_for_win(state[i][0], state[i][1], state[i][2]):
-            return from_symbol_to_player(state[i][0])
+            winners_symbol = state[i][0]
     # check the columns
     for i in range(0, len(state)):
         if check_squares_for_win(state[0][i], state[1][i], state[2][i]):
-            return from_symbol_to_player(state[0][i])
+            winners_symbol = state[0][i]
     # check the diagonals
     # top-left to bottom-right
     if check_squares_for_win(state[0][0], state[1][1], state[2][2]):
-        return from_symbol_to_player(state[0][0])
+        winners_symbol = state[0][0]
     # top-right to bottom-left
     if check_squares_for_win(state[0][2], state[1][1], state[2][0]):
-        return from_symbol_to_player(state[2][0])
-    return None
+        winners_symbol = state[0][2]
+    # if a winner has been found, return the winner's name
+    if winners_symbol == 'x':
+        return 'player_1'
+    elif winners_symbol == 'o':
+        return 'player_2'
+    else:
+        return None
 
-# check to see if a sequence of squares have equal values
+# check to see if a sequence of squares have the same symbol
 def check_squares_for_win(x, y, z):
-    is_win = False
-    # make sure at least one value isn't None
-    if x:
-        # check that all 3 values are equal
-        if x == y and y == z:
-            is_win = True
-    return is_win
+    # make sure at least one value isn't None, and that all values are equal
+    if x and x == y and y == z:
+            return True
+    return False
 
 # check if the state is full with symbols
 def is_full_state(state):
@@ -161,20 +155,26 @@ def is_full_state(state):
                 return False
     return True
 
+def play():
 
-# initializations --------------------------------------------------------------
-player_symbols = {'player_1': 'x', 'player_2': 'o'}
-player_type = {'player_1': 'human', 'player_2': 'ai'}
-state = init_state()
-turn_of = 'player_1'
-game_over = False
+    # initializations for human vs machine game ------------------------------------
+    player_symbols = {'player_1': 'x', 'player_2': 'o'}
+    player_type = {'player_1': 'human', 'player_2': 'ai'}
+    state = init_state()
+    turn_of = 'player_1'
+    game_over = False
+
+    # state = init_state()
+    # game_node = GameNode(state, turn_of, None)
+    # game_node.generate_game_tree()
+
+    # game loop --------------------------------------------------------------------
+    while not game_over:
+        (x,y) = get_move(turn_of, player_type, state) # loop until a valid move is selected
+        state[x][y] = player_symbols[turn_of]
+        turn_of = switch_player(turn_of)
+        print_state(state)
+        game_over = terminal_test(state)
 
 
-
-# game loop --------------------------------------------------------------------
-while not game_over:
-    (x,y) = get_move(turn_of, state) # loop until a valid move is selected
-    state[x][y] = player_symbols[turn_of]
-    print_state(state)
-    game_over = check_if_done(state)
-    turn_of = switch_player(turn_of)
+play()
